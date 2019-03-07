b993b097427a407fc2c6c1175ef74d7054f2ef24
PatrickIckes <patrick.ickes@loop.colum.edu>
2019-03-07 15:01:24 -0600

    end of tutorial 2

---
+++ Assets/Scripts/InteractWithLookedAt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Detects when the player presses the Interact button when looking at and IInteractive and then calls the IInteractive's InteractWith method.
+/// </summary>
+public class InteractWithLookedAt : MonoBehaviour
+{
+    [SerializeField]
+    private DetectLookedAtInteractive detectLookedAtInteractive;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact") && detectLookedAtInteractive.lookedAtInteractive != null)
+            Debug.Log("Player pressed the Interact button.");
+        detectLookedAtInteractive.lookedAtInteractive.InteractWith();
+    }
+}
