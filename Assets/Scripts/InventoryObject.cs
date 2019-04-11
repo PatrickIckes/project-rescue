59c8bb75963b23f2cc9c2093ace6a83482003c9b
PatrickIckes <patrick.ickes@loop.colum.edu>
2019-04-10 23:53:14 -0500

    end of tutorial 15

--- Assets/Scripts/InventoryObject.cs
+++ Assets/Scripts/InventoryObject.cs
@@ -4,23 +4,38 @@ using UnityEngine;
 
 public class InventoryObject : InteractiveObject
 {
+    [Tooltip("The name of the object, as it will appear in the inventory menu UI.")]
+    [SerializeField]
+    private string objectName = nameof(InventoryObject);
+
     // TODO: Add long description field
     // TODO: Add icon field
 
+    private new Renderer renderer;
+    private new Collider collider;
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        collider = GetComponent<Collider>();
+    }
+
     public InventoryObject()
     {
-        displayText = nameof(InventoryObject);
+        displayText = $"Take {objectName}";
     }
 
     /// <summary>
     /// When the player interacts with an inventory object, we need to do 2 things:
     /// 1. Add the inventory object to the PlayerInventory list
-    /// 2. Remove the object from the game world / scene
+    /// 2. Remove the object from the game world / scene.
+    /// 
     /// </summary>
     public override void InteractWith()
     {
         base.InteractWith();
         PlayerInventory.InventoryObjects.Add(this);
-
+        collider.enabled = false;
+        renderer.enabled = false;
     }
 }
