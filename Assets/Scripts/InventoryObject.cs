7fa207b013b3bdf14927bbcf17595cdecd125d84
PatrickIckes <patrick.ickes@loop.colum.edu>
2019-04-20 22:57:27 -0500

    end of tutorial 25 - inventory function part 5

--- Assets/Scripts/InventoryObject.cs
+++ Assets/Scripts/InventoryObject.cs
@@ -17,6 +17,8 @@ public class InventoryObject : InteractiveObject
     [SerializeField]
     private Sprite icon;
 
+    public Sprite Icon => icon;
+    public string Description => description;
     public string ObjectName => objectName;
 
     private new Renderer renderer;
@@ -43,6 +45,7 @@ public class InventoryObject : InteractiveObject
     {
         base.InteractWith();
         PlayerInventory.InventoryObjects.Add(this);
+        InventoryMenu.Instance.AddItemToMenu(this);
         collider.enabled = false;
         renderer.enabled = false;
         Debug.Log($"Inventory menu game object name {InventoryMenu.Instance.name}");
